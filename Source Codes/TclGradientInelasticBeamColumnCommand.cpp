5969bb817dd04ca2cfd8ef3fea379a4feecd4386
Mohammad Salehi <mohammad.salehi@tamu.edu>
2021-04-13 23:44:16 -0500

    Update TclGradientInelasticBeamColumnCommand.cpp

--- Source Codes/TclGradientInelasticBeamColumnCommand.cpp
+++ Source Codes/TclGradientInelasticBeamColumnCommand.cpp
@@ -1,24 +1,6 @@
-/* ****************************************************************** **
-**    OpenSees - Open System for Earthquake Engineering Simulation    **
-**          Pacific Earthquake Engineering Research Center            **
-**                                                                    **
-**                                                                    **
-** (C) Copyright 1999, The Regents of the University of California    **
-** All Rights Reserved.                                               **
-**                                                                    **
-** Commercial use of this program without express permission of the   **
-** University of California, Berkeley, is strictly prohibited.  See   **
-** file 'COPYRIGHT'  in main directory for information on usage and   **
-** redistribution,  and for a DISCLAIMER OF ALL WARRANTIES.           **
-**                                                                    **
-** Developed by:                                                      **
-**   Frank McKenna (fmckenna@ce.berkeley.edu)                         **
-**   Gregory L. Fenves (fenves@ce.berkeley.edu)                       **
-**   Filip C. Filippou (filippou@ce.berkeley.edu)                     **
-**                                                                    **
-** ****************************************************************** */
-
 /* Written by: Mohammad Salehi (mohammad.salehi@tamu.edu)
+** (C) Copyright 2021, Mohammad Salehi and Petros Sideris
+** All rights reserved
 ** Created: 07/19
 ** Description: The source code for the Tcl commands of gradient inelastic (GI) force-based beam-column element formulation
 */
